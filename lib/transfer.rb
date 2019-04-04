91e1010871470aeeebc2da5cf8c7bcc6db7d0a63
ferrisboran <ferris@ferryrules.com>
2019-04-04 11:13:40 -0400

    done!

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,9 +26,11 @@ class Transfer
   end
 
   def reverse_transfer
-    self.sender.deposit(@amount)
-    self.receiver.deposit(-@amount)
-    self.status = "reversed"
+    if self.status == "complete"
+      self.sender.deposit(@amount)
+      self.receiver.deposit(-@amount)
+      self.status = "reversed"
+    end
   end
 
 end
